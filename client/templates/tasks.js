4602e113cfcb344c5c079f5c79062dcdfe3a0c5d
Collin L. Walling <collin.walling@gmail.com>
2016-04-10 02:39:52 -0400

    Tasks are displayed from database.
    Task view can query database and display the task name
    along with any tags. These tags do not relate to the task
    yet.

---
+++ client/templates/tasks.js
@@ -0,0 +1,16 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../../imports/db/collections.js';
+import { Tags } from '../../imports/db/collections.js';
+
+import './maincontent.html';
+
+Template.maincontent.helpers({
+
+    tasks() {
+        return Tasks.find({});
+    },
+    tags() {
+        return Tags.find({});
+    },
+});
