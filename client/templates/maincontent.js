bcb918486dccccdae6fc5a82d62ff0878af02c98
Collin L. Walling <collin.walling@gmail.com>
2016-04-10 05:37:28 -0400

    Added search functionality to tasks.
    Tasks are searched as you type.

--- client/templates/maincontent.js
+++ client/templates/maincontent.js
@@ -6,13 +6,28 @@ import { Tags } from '../../imports/db/collections.js';
 import './maincontent.html';
 import './maincontentModal.html';
 
+/*
+  Helpers for displaying content from DB.
+*/
 Template.maincontent.helpers({
+  /*
+    Displays all tasks based on search parameters, if any.
+  */
+  tasks() {
+    var search = Session.get('search');
 
-    tasks() {
-        return Tasks.find({});
-    },
+    // if there is something in the search bar, search for it!
+    if (search !== null)
+      return Tasks.find({task:search});
+    // else return everything in the collection
+    else
+      return Tasks.find({});
+  },
 });
 
+/*
+  Events for dynamic app control.
+*/
 Template.maincontent.events({
   'click #add': function(e) {
     e.preventDefault();
@@ -20,5 +35,20 @@ Template.maincontent.events({
     jQuery.noConflict();
 
     Modal.show('maincontentModal');
+  },
+
+  /*
+    Search functionality. Searches as you type.
+  */
+  'keyup #maincontent_container': function(e) {
+    e.preventDefault();
+    var search = e.target.value;
+
+    // if the search box is not empty, set the session variable
+    if (search !== "")
+      Session.set('search', search);
+    // else set the session variable to null so we can see all the tasks
+    else
+      Session.set('search', null);
   }
 });
